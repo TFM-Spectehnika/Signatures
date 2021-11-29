0554842340daaf78d14806aee10d1c08ce4eb5d1
Kurlis Andrey <94551812+Warikos@users.noreply.github.com>
2021-11-29 09:02:54 +0700

    Update outlooksignature.ps1

--- outlooksignature.ps1
+++ outlooksignature.ps1
@@ -1,18 +1,16 @@
 Clear-Host
-#Set Company and share variables
+#Set Varibles
 $CompanyName = "TFM-Spectehnika"
-$DomainName = "krut.ru"
-$SigSource = "\\net_share\Signatures"
+$SigSource = "\\srv-db1\Config.1C$\Signatures"
 
-#Set System Variables
+#Connect to ad and set Signatures Path
 $AppData=(Get-Item env:appdata).value
 $SigPath = "\Microsoft\Signatures"
 $LocalSignaturePath = $AppData+$SigPath
-$RemoteSignaturePathFull = $SigSource+"\"+$CompanyName+".docx"
 $fullPath = $LocalSignaturePath+"\"+$CompanyName+".docx"
 $fullPathHTM = $LocalSignaturePath+"\"+$CompanyName+".htm"
 
-#Connect and get ad data
+#Get User information
 $UserName = $env:username
 $Filter = "(&(objectCategory=User)(samAccountName=$UserName))"
 $Searcher = New-Object System.DirectoryServices.DirectorySearcher
@@ -26,7 +24,7 @@ $ADTelePhoneNumber = $ADUser.TelephoneNumber
 $ADDepartment = $ADUser.Department
 $CompanyRegPath = "HKCU:\Software\"+$CompanyName
 
-#Function detect keyvalue in regestry
+#Check reg
 function isRegistryValue {
     param (
         [parameter(Mandatory = $true)]
@@ -41,19 +39,19 @@ function isRegistryValue {
 }
 
 
-#Function delete path
-Function DELETE_PATH { 
+#Delete Folder
+Function DELETE_Folder { 
 If (Test-Path -Path $LocalSignaturePath) {
 Remove-Item -Path $LocalSignaturePath -Recurse
 }}
 
-#Delete registry path
+#Delete REG
 Function DELETE_REG {
 If (Test-Path -Path $CompanyRegPath) {
-Remove-Item –Path $CompanyRegPath –Recurse
+Remove-Item -Path $CompanyRegPath -Recurse
 }}
 
-#Create regestry path
+#Create REG PATH
 Function CREATE_REG {
 if (Test-Path $CompanyRegPath)
 {}
@@ -66,10 +64,10 @@ else
 {New-Item -path $CompanyRegPath -name "OutlookSignatureSettings"}
 }
 
-#Function set default signatures
+#Set main signatures
 Function Main_Sign {
 if (isRegistryValue -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Setup" -ValueName ImportPRF) {
-    $currentState = (Get-ItemProperty –Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Setup" -Name $key.Value_Name).$( $key.Value_Name )
+    $currentState = (Get-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Setup" -Name $key.Value_Name).$( $key.Value_Name )
     if (($currentState -eq $key.Value) -or (([string]::IsNullOrEmpty($key.Value)) -and ([string]::IsNullOrEmpty($currentState))))
     {}
     else {
@@ -85,24 +83,21 @@ else {
 }
 }
 
-#Set regestry data
+#Set data to reg
 Function REG_DATA {
-$SigVersion = (gci $RemoteSignaturePathFull).LastWriteTime #check signature version
-$SignatureVersion = (Get-ItemProperty $CompanyRegPath"\OutlookSignatureSettings").SignatureVersion
 Set-ItemProperty $CompanyRegPath"\OutlookSignatureSettings" -name SignatureSourceFiles -Value $SigSource
 New-ItemProperty $CompanyRegPath"\OutlookSignatureSettings" -name Title -PropertyType String -Value $ADTitle
 New-ItemProperty $CompanyRegPath"\OutlookSignatureSettings" -name EmailAddress -PropertyType String -Value $ADEmailAddress
 New-ItemProperty $CompanyRegPath"\OutlookSignatureSettings" -name Department -PropertyType String -Value $ADDepartment
 New-ItemProperty $CompanyRegPath"\OutlookSignatureSettings" -name TelePhoneNumber -PropertyType String -Value $ADTelePhoneNumber
-$SignatureSourceFiles = (Get-ItemProperty $CompanyRegPath"\OutlookSignatureSettings").SignatureSourceFiles
 }
 
-#Generate signatures
+#Create signatures
 Function Create_SIGN { 
 
 Copy-Item -Path $SigSource $AppData"\Microsoft" -Recurse -Force
 
-#Variables for New-Object -com word.application
+#Set Enviroment for New-Object -com word.application
 $ReplaceAll = 2
 $FindContinue = 1
 $MatchCase = $True
@@ -114,7 +109,7 @@ $Forward = $True
 $Wrap = $FindContinue
 $Format = $False
 
-#Run word applet and replace data
+#Start applet
 $MSWord = New-Object -com word.application
 #$MSWord.Visible = $True
 $MSWord.Documents.Open($fullPath)
@@ -138,9 +133,9 @@ $MSWord.Selection.Find.Execute($FindText, $MatchCase, $MatchWholeWord, $MatchWil
 $MSWord.Selection.Find.Execute("Email")
 $MSWord.ActiveDocument.Hyperlinks.Add($MSWord.Selection.Range, "mailto:"+$ADEmailAddress.ToString(), $missing, $missing, $ADEmailAddress.ToString())
 
-#Save document
+#Save chenged documents
 $MSWord.ActiveDocument.Save()
-#Generate HTM
+#Save to HTM
 $saveFormat = [Enum]::Parse([Microsoft.Office.Interop.Word.WdSaveFormat], "wdFormatHTML");
 [ref]$BrowserLevel = "microsoft.office.interop.word.WdBrowserLevel" -as [type]
 $MSWord.ActiveDocument.WebOptions.OrganizeInFolder = $true
@@ -149,15 +144,15 @@ $MSWord.ActiveDocument.WebOptions.BrowserLevel = $BrowserLevel::wdBrowserLevelMi
 $path = $LocalSignaturePath+"\"+$CompanyName+".htm"
 $MSWord.ActiveDocument.saveas([ref]$path, [ref]$saveFormat)
 
-#Generate RTF
+#Save to RTF
 $saveFormat = [Enum]::Parse([Microsoft.Office.Interop.Word.WdSaveFormat], "wdFormatRTF");
 $path = $LocalSignaturePath+"\"+$CompanyName+".rtf"
 $MSWord.ActiveDocument.SaveAs([ref] $path, [ref]$saveFormat)
 
-#Generate TXT
+#Save to TXT
 $saveFormat = [Enum]::Parse([Microsoft.Office.Interop.Word.WdSaveFormat], "wdFormatText");
 
-#Old code
+#Needn't 
 #$path = $LocalSignaturePath+"\"+$CompanyName+".rtf"
 #$MSWord.ActiveDocument.SaveAs([ref] $path, [ref]$saveFormat)
 
@@ -167,37 +162,39 @@ $MSWord.ActiveDocument.Close()
 $MSWord.Quit()
 }
 
-#Main massiv
+#Check all
 if ($ADTitle -eq ($Title = (Get-ItemProperty $CompanyRegPath"\OutlookSignatureSettings").Title))
     {if ($ADDepartment -eq ($Department = (Get-ItemProperty $CompanyRegPath"\OutlookSignatureSettings").Department))
         {if ($ADTelePhoneNumber -eq ($TelePhoneNumber = (Get-ItemProperty $CompanyRegPath"\OutlookSignatureSettings").TelePhoneNumber))
             {if (Test-Path -PathType Leaf -Path $fullPathHTM)
-                {Write-Host "1"
-                Main_Sign
+                {Main_Sign
                 }
-            else{DELETE_REG
+            else{
+            DELETE_Folder
+            DELETE_REG
             CREATE_REG
-            Write-Host "2"
             REG_DATA
             Create_SIGN
             Main_Sign}}
-        else {DELETE_REG
+        else {
+        DELETE_Folder    
+        DELETE_REG
         CREATE_REG
-        Write-Host "3"
         REG_DATA
         Create_SIGN
         Main_Sign}}
-     else {DELETE_REG
-     CREATE_REG
-     Write-Host "4"
-     REG_DATA
-     Create_SIGN
-     Main_Sign}}
-else {DELETE_REG
+    else {
+    DELETE_Folder
+    DELETE_REG
+    CREATE_REG
+    REG_DATA
+    Create_SIGN
+    Main_Sign}}
+else {
+DELETE_Folder
+DELETE_REG
 CREATE_REG
-Write-Host "5"
 REG_DATA
 Create_SIGN
 Main_Sign}
-
-exit
+Exit
